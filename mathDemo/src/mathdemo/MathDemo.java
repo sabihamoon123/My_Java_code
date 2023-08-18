ea3e4f5e853f3f1b4ab39fc91188aff94e8d4c11
Sabiha Mahjabin Moon <115579991+sabihamoon123@users.noreply.github.com>
2023-08-18 20:32:14 +0600

    Add files via upload

--- mathDemo/src/mathdemo/MathDemo.java
+++ mathDemo/src/mathdemo/MathDemo.java
@@ -10,41 +10,57 @@ package mathdemo;
  */
 public class MathDemo {
 
-   
     public static void main(String[] args) {
         
-        for (int i=10 ; i>=1 ; i--){
-            System.out.println(i) ;
+        int sum = 0  ;
+        for(int i=1 ; i<=10 ; i++){
+            sum=sum+i ;
+           
         }
-        for (int i=2 ; i<=100 ; i=i+2){
+         System.out.println("Sum = " +sum);
+        
+        
+        int i=1 ;
+        do{
             System.out.println(i) ;
+            i=i+2 ;
+       }while(i<=100);
+        
+        
+
+        for ( i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+        for ( i = 2; i <= 100; i = i + 2) {
+          System.out.println(i);
+       }
+
+      //  int i= 2 ;
+        while(i<=100){
+            System.out.println(i);
+            i=i+2 ;
         }
         
+        int x=2 ;
+        int y=3 ;
         
+        int max = Math.max(x, y) ;
+        System.out.println("Maximum " +max) ;
         
+        int min = Math.min(x, y) ;
+        System.out.println("Minimum " +min) ;
         
+     int absolute = Math.abs(y) ;
+        System.out.println("Absolute of Y " +absolute) ;
         
+        double power = Math.pow(x, y) ;
+        System.out.println("x to the power y " +power) ;
         
-//        int x=2 ;
-//        int y=3 ;
-//        
-//        int max = Math.max(x, y) ;
-//        System.out.println("Maximum " +max) ;
-//        
-//        int min = Math.min(x, y) ;
-//        System.out.println("Minimum " +min) ;
-//        
-//        int absolute = Math.abs(y) ;
-//        System.out.println("Absolute of Y " +absolute) ;
-//        
-//        double power = Math.pow(x, y) ;
-//        System.out.println("x to the power y " +power) ;
-//        
-//        int round = Math.round(8.8f) ;
-//        System.out.println("round of 8.8 ="+round);
-//        
-//        double pi = Math.PI ;
-//        System.out.println(pi);
+        int round = Math.round(8.8f) ;
+        System.out.println("round of 8.8 ="+round);
+        
+        double pi = Math.PI ;
+       System.out.println(pi);
     }
-    
+
 }
