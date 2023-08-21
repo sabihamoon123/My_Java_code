3a0567bc66ffc537f15bd7b05b47bac3f81b8db7
Sabiha Mahjabin Moon <115579991+sabihamoon123@users.noreply.github.com>
2023-08-21 20:01:32 +0600

    Add files via upload

---
+++ Divisible/src/divisible/Divisible.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package divisible;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Divisible {
+
+    
+    public static void main(String[] args) {
+        
+        
+         for (int i = 1 ; i<=100 ; i=i+3){
+        if(i  == 10){
+            continue ;
+        }
+        if(i>13){
+            break ;     
+        }
+        
+        System.out.println(i) ;
+    }
+        
+       System.out.println("Diveided by 3 =") ;
+        for (int i = 1 ; i<=100 ; i++){
+//        if(i % 3 == 0){
+//            System.out.println(i) ;
+//        }
+//    }
+//        System.out.println("Divided by 5 =") ;
+//         for (int i = 1 ; i<=100 ; i++){
+//        if(i % 5 == 0){
+//            System.out.println(i) ;
+//        }
+//    }
+//         System.out.println("Divided by 3 & 5 = ") ;
+//          for (int i = 1 ; i<=100 ; i++){
+//        if(i % 3 == 0 && i % 5 == 0){
+//            System.out.println(i) ;
+//        }
+//   }
+        
+    }
+    
+}
